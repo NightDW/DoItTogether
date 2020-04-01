dcbb96b910dc47ba53fb0c605df1f578b2f79f6c
NightDW <2508898454@qq.com>
2020-04-01 11:25:24 +0800

    Finish the function of forbidden words

--- src/main/java/com/laidw/web/controller/MessageController.java
+++ src/main/java/com/laidw/web/controller/MessageController.java
@@ -1,10 +1,10 @@
 package com.laidw.web.controller;
 
+import com.laidw.service.MembershipService;
 import com.laidw.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -16,9 +16,15 @@ public class MessageController extends BaseController {
     @Autowired
     private MessageService messageService;
 
+    @Autowired
+    private MembershipService membershipService;
+
     @PostMapping
     @ResponseBody
     public String doSendMessage(Boolean isPrivate, Integer senderId, Integer acceptorId, String content){
+        //如果是群成员发送消息，则先判断他是否被禁言了
+        if(!isPrivate && membershipService.selectMembership(getCurrentUser().getId(), acceptorId, true).getIsMute())
+            return "Failed";
         String result = messageService.sendMsg(isPrivate, senderId, acceptorId, content) + ";";
         result += new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
         return result;
